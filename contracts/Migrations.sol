3ff1e84b0f0c6e1251c0637808f94e2fe4391017
Uwe Voelker <uwe.voelker@xing.com>
2021-08-04 15:56:47 +0200

    manual changes (error issues)

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract Migrations {
     address public owner = msg.sender;
-    uint256 public last_completed_migration;
+    uint256 public lastCompletedMigration;
 
     modifier restricted() {
         require(
@@ -14,6 +14,6 @@ contract Migrations {
     }
 
     function setCompleted(uint256 completed) public restricted {
-        last_completed_migration = completed;
+        lastCompletedMigration = completed;
     }
 }
